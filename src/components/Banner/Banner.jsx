9a351d6d0bd4bdadf6912cb45ad6756f87fd8631
tmuzikova <tereza.muzikova@gmail.com>
2024-06-26 19:21:25 +0200

    divided content into components

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import "./Banner.css";
+
+export const Banner = () => {
+  return (
+    <section className="banner" id="home">
+      <div className="container">
+        <div className="banner__content">
+          <h1>Přijď to ochutnat!</h1>
+          <p>Nabízíme kvalitní kávu, útulné prostředí a perfektní servis.</p>
+        </div>
+      </div>
+    </section>
+  );
+};
